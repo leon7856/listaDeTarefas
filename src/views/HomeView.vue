49f2d099e8fa7b307f75c0d7d287bd6ca52cc51f
leon7856 <luanlleon2006@gmail.com>
2024-03-02 15:21:16 -0300

    Adição de uma nova rota, criação de um componente To do List, Alteração da HomeView.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="py-4"> Lista de Projetos</h1>
+    <div class="row row-cols-3">
+      <div class="col center-div">
+        <div class="card" style="width: 85%;">
+          <img src="../assets/mclaren.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">To Do List</h5>
+            <p class="card-text">A simple to-do list made using Vue.js.</p>
+            <router-link class="d-grid gap-2" to="/list">
+              <button href="#" class="btn btn-primary">Access project</button>
+            </router-link>
+          </div>
+        </div>
+      </div>
+      <div class="col center-div">
+        <div class="card" style="width: 85%;">
+          <img src="../assets/mclaren.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div class="d-grid gap-2">
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="col center-div">
+        <div class="card" style="width: 85%;">
+          <img src="../assets/mclaren.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div class="d-grid gap-2">
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+     
+
+    </div>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
+
+<style>
+  .center-div {
+    display: flex;
+    justify-content: center;
+  }
+
+
+
+</style>
